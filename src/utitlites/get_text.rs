f89b3d98223e041eb1b6ee5e62839d5169f5ae6a
Vaaris16 <vaarischitkara@Vaaris16s-MacBook-Pro.local>
2026-06-06 19:02:14 +0530

    removed unused fonts and minor changes to get_text

--- src/utitlites/get_text.rs
+++ src/utitlites/get_text.rs
@@ -2,5 +2,5 @@ pub fn get_time(total_time: usize) -> String {
     let secs = total_time % 60;
     let min = total_time / 60;
 
-    format!("{:02}:{:02}", min, secs)
+    format!("{:02} : {:02}", min, secs)
 }
