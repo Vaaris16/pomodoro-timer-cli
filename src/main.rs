efc852cecab1fdb41d9a5f714c690e0c7b2a98d0
Vaaris16 <vaarischitkara@Vaaris16s-MacBook-Pro.local>
2026-06-06 12:33:47 +0530

    used figlet text instead of default text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{thread, time::Duration};
 
 use crossterm::terminal;
+use figlet_rs::FIGlet;
 
 const WORK_TIME: usize = 25 * 60;
 const REST_TIME: usize = 10 * 60;
@@ -45,17 +46,24 @@ fn get_time(total_time: usize) -> String {
     format!("{:02}:{:02}", min, secs)
 }
 
-fn center(time: &str) {
+fn center(time: &str, font: FIGlet) {
+    let rendered = font.convert(time).unwrap().to_string();
+    let lines: Vec<&str> = rendered.lines().collect();
+
+    let tw = lines.iter().map(|l| l.len()).max().unwrap();
+
     let (cols, rows) = terminal::size().unwrap();
 
-    let pad_x = (cols as usize).saturating_sub(time.len()) / 2;
+    let pad_x = (cols as usize).saturating_sub(tw) / 2;
     let pad_y = (rows as usize).saturating_sub(time.len()) / 2;
 
     print!("\x1B[2J\x1B[1;1H");
 
     print!("{}", "\n".repeat(pad_y));
 
-    println!("{}{}", " ".repeat(pad_x), time);
+    for line in lines {
+        println!("{}{}", " ".repeat(pad_x), line);
+    }
 }
 
 fn change_state(pomodoro: &mut Pomodoro) {
@@ -71,9 +79,10 @@ fn main() {
     let mut pomodoro = Pomodoro::new();
 
     loop {
+        let big_font = FIGlet::big().unwrap();
         let time = get_time(pomodoro.total_time);
 
-        center(&time);
+        center(&time, big_font);
 
         change_state(&mut pomodoro);
 
